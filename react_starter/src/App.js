a44bbf2d3b5f9cc08e20d08f15ece404328aba5e
K-Edmeade <kya.edmeade@gmail.com>
2022-08-30 15:54:23 +0100

    initial Commit

---
+++ react_starter/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div >
+      <header>
+        <h1>Boston Celtics</h1>
+        <h3>17 Time World Champions</h3>
+        <p>
+          2022 Eastern Conference Champions
+        </p>
+        
+       
+      </header>
+
+      <body>
+      <h3>Roster</h3>
+      <h5>Guards</h5>
+        <ul>
+        <li>Marcus Smart</li>
+        <li>Malcolm Brogdon</li>
+        <li>Payton Pritchard</li>
+        <li>Jaylen Brown</li>
+        <li>JD Davidson</li>
+        <li>Derrick White</li>
+        </ul>
+      <h5>Forwards</h5>
+        <ul>
+        <li>Jayson Tatum</li>
+        <li>Danilo Gallinari</li>
+        <li>Sam Hauser</li>
+        <li>Noah Vonleh</li>
+        <li>Grant Williams</li>
+        <li>Bruno Caboclo</li>
+        </ul>
+      <h5>Center</h5>
+        <ul>
+        <li>Al Horford</li>
+        <li>Mfiondu Kabengele</li>
+        <li>Robert Williams III</li>
+        </ul>
+       
+      </body>
+    </div>
+  );
+}
+
+
+export default App;
